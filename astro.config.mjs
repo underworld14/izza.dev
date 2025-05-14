ec06fbf3fcc325f623b3e7e2c65a83469878d819
Yusril Izza Aulia <yusril@izza.dev>
2025-05-14 23:19:21 +0700

    feat: add Cloudflare Pages deployment configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
   },
 
   integrations: [react()],
+  adapter: cloudflare(),
 });
